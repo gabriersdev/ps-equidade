29eed4652244713c2bab11228470b6b73193c45e
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2026-09-21 20:47:20 -0300

    fix: update calendar events and remove commented entry

--- resources/calendar-events.ts
+++ resources/calendar-events.ts
@@ -12,14 +12,13 @@ export const events: CalendarEvent[] = [
   {date: '11/07/2026', title: 'Aula 3 - Scratch', id: "scratch-aula-3", timeInit: "09:00:00", timeFinish: "11:00:00",},
   {date: '08/08/2026', title: 'Aula 4 - Scratch', id: "scratch-aula-4", timeInit: "09:00:00", timeFinish: "11:00:00",},
   {date: '29/08/2026', title: 'Aula 5 - Scratch', id: "scratch-aula-5", timeInit: "09:00:00", timeFinish: "11:00:00",},
-  // {date: '29/08/2026', title: 'Aula 6 - Lego/Robótica', timeInit: "09:00:00", timeFinish: "11:00:00",},
   {date: '12/09/2026', title: 'Aula 7 - Lego/Robótica', id: "robotica-aula-7", timeInit: "09:00:00", timeFinish: "11:30:00",},
   {date: '19/09/2026', title: 'Aula Extra - Lego/Robótica', timeInit: "09:00:00", timeFinish: "11:00:00",},
   {date: '26/09/2026', title: 'Aula 8 - Lego/Robótica', timeInit: "09:00:00", timeFinish: "11:30:00",},
-  {date: '03/10/2026', title: 'Aula 9 - Lego/Robótica', timeInit: "09:00:00", timeFinish: "11:30:00",},
+  {date: '03/10/2026', title: 'Aula 9 - Oficina Segurança Digital - METIS', timeInit: "09:00:00", timeFinish: "11:30:00",},
   {date: '24/10/2026', title: 'Aula 10 - Cinema Comentado', timeInit: "09:00:00", timeFinish: "11:30:00",},
   {date: '07/11/2026', title: 'Aula 11 - Python', timeInit: "09:00:00", timeFinish: "11:00:00",},
   {date: '14/11/2026', title: 'Aula 12 - Python', timeInit: "09:00:00", timeFinish: "11:00:00",},
-  {date: '28/11/2026', title: 'Aula 14 - Python', timeInit: "09:00:00", timeFinish: "11:00:00",},
+  {date: '28/11/2026', title: 'Aula 13 - Python', timeInit: "09:00:00", timeFinish: "11:00:00",},
   {date: '05/12/2026', title: 'Formatura', timeInit: "09:00:00", timeFinish: "12:00:00",}
 ];
